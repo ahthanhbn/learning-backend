1d4297e310f6ebbe33961847d31665b7f6f7651b
Nguyễn Bá Thành <acebn02@gmail.com>
2025-11-17 16:03:33 +0700

    buoi 06

--- src/server.js
+++ src/server.js
@@ -3,7 +3,9 @@ require("dotenv").config();
 const express = require("express");
 const path = require("path");
 const configViewEngine = require("./config/viewEngine");
-webRoutes = require("./routes/web");
+const webRoutes = require("./routes/web");
+const mysql = require("mysql2");
+
 const app = express();
 const port = process.env.PORT || 8888;
 const hostname = process.env.HOST_NAME;
@@ -11,8 +13,27 @@ const hostname = process.env.HOST_NAME;
 //config temple engine
 configViewEngine(app);
 
-//điều hướng 
+//điều hướng
 app.use("/", webRoutes);
+
+//test connection
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3307, //default: 3306
+  user: "root",
+  password: "123456",
+  database: "hoidanit",
+});
+
+connection.connect();
+
+connection.query("SELECT * FROM Users u ;", function (error, results, fields) {
+  if (error) throw error;
+  console.log("🚀 ~ results:", results);
+  console.log("🚀 ~ fields:", fields);
+});
+
+connection.end();
 
 // cách viết sau để chia api version
 // app.use("/test", webRoutes);
